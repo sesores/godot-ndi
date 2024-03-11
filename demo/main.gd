ce9bf5a2d41a54e7ad58912a4c78fce20697901f
sesores <sesores@gmail.com>
2024-03-11 06:23:39 +0200

    feat(demo): updated demo code

--- demo/main.gd
+++ demo/main.gd
@@ -3,15 +3,28 @@ extends Node3D
 
 
 @onready var ndi:NDIInput = $NDIInput
+@onready var audio_player:AudioStreamPlayer = $AudioStreamPlayer
 @onready var mesh:MeshInstance3D = $MeshInstance3D
 
 @onready var enable_rotation:CheckButton = %EnableRotation
 
 var material:StandardMaterial3D
 
+var samples := PackedFloat32Array()
+var audio_playback:AudioStreamGeneratorPlayback = null
+var sample_hz:float = 440.0
+var pulse_hz:float = 440.0
+
+
 
 
 func _ready():
+	sample_hz = audio_player.stream.mix_rate
+	
+	audio_player.play()
+	audio_playback = audio_player.get_stream_playback()
+	fill_audio_buffer()
+	
 	material = mesh.get_active_material(0) as StandardMaterial3D
 	material.albedo_texture = ndi.get_texture()
 	
@@ -24,6 +37,13 @@ func _ready():
 
 
 func _process(_delta):
+	# AUDIO
+	#var frames_available = audio_playback.get_frames_available()
+	#print("AUDIO: ", frames_available)
+	#for i in range(frames_available):
+		#audio_playback.push_frame(Vector2.ZERO)
+	
+	# ROTATION
 	if (enable_rotation.button_pressed):
 		mesh.rotate_y(_delta * 0.3)
 
@@ -36,9 +56,28 @@ func on_video_frame(_frame:NDIVideoFrame):
 
 
 func on_audio_frame(_frame:NDIAudioFrame):
-	#print("FRAME: ", _frame.get_frame_rate(), ", ", _frame.get_original_size(), ", ", _frame.get_image().get_size())
-	pass
+	#samples.append_array(_frame.get_samples())
+	
+	var available = audio_playback.get_frames_available()
+	var samples:PackedFloat32Array = _frame.get_samples()
+	
+	var count:int = mini(_frame.get_sample_count() / _frame.get_channel_count(), available)
+	
+	print("AUDIO FRAME: ", available, " x ", _frame.get_sample_count())
+	
+	for i in range(count):
+		audio_playback.push_frame(Vector2(samples[i], 0.0))
 
+
+
+func fill_audio_buffer():
+	var phase = 0.0
+	var increment = pulse_hz / sample_hz
+	var frames_available = audio_playback.get_frames_available()
+	
+	for i in range(frames_available):
+		audio_playback.push_frame(Vector2.ONE * sin(phase * TAU))
+		phase = fmod(phase + increment, 1.0)
 
 
 func on_meta_frame(_frame:NDIMetaFrame):
